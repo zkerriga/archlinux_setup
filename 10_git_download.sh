a639b4cdcea9ff01ff490d541c490d7b95d9a24a
zkerriga <daniil.63@yandex.ru>
2020-06-13 22:16:58 +0300

    Git_download fixed

--- 10_git_download.sh
+++ 10_git_download.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
-git clone https://aur.archlinux.org/rtl8821ce-dkms-git.git	/opt/
+git clone https://aur.archlinux.org/rtl8821ce-dkms-git.git	/opt/rtl8821ce-dkms-git
 echo -e "\033[32m[+]\033[0m The driver repository for the Internet has been downloaded!"
 
-git clone https://aur.archlinux.org/ttf-monaco.git			/opt/
+git clone https://aur.archlinux.org/ttf-monaco.git			/opt/ttf-monaco
 echo -e "\033[32m[+]\033[0m The ttf-monaco has been downloaded!"
