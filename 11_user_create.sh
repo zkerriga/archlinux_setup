8b7e88b055a39b89f84da3cb1ce911b50006565c
zkerriga <daniil.63@yandex.ru>
2020-06-14 20:58:31 +0300

    Add visudo-vim

--- 11_user_create.sh
+++ 11_user_create.sh
@@ -6,6 +6,7 @@ useradd -m -g users -G wheel -s /bin/bash $name
 passwd $name
 
 pacman -S sudo
+ln -s /usr/bin/vim /usr/bin/vi
 
 echo -e "\033[32m[+]\033[0m The user is created!"
 echo -e "\033[33m[!]\033[0m Please, start \033[34mvisudo\033[0m!"
