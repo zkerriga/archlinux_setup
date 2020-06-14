e0f90aeedf30ea4fc842596d6147f035e2ad56c4
zkerriga <daniil.63@yandex.ru>
2020-06-14 21:40:31 +0300

    Add xbindkeys

--- 10_git_download.sh
+++ 10_git_download.sh
@@ -3,5 +3,8 @@
 git clone https://aur.archlinux.org/rtl8821ce-dkms-git.git	/opt/rtl8821ce-dkms-git
 echo -e "\033[32m[+]\033[0m The driver repository for the Internet has been downloaded!"
 
+git clone https://aur.archlinux.org/xbindkeys.git		/opt/xbindkeys
+echo -e "\033[32m[+]\033[0m Xbindkeys downloaded"
+
 git clone https://aur.archlinux.org/ttf-monaco.git			/opt/ttf-monaco
 echo -e "\033[32m[+]\033[0m The ttf-monaco has been downloaded!"
